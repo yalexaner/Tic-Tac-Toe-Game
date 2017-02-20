64554f946a4ab82314695bee50956af8e2817ebb
yalexaner <yachmenev.aleksandr2000@gmail.com>
2017-02-20 19:41:39 +0500

    Adding new graphic output with OOP

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-import os # system("clean")
+import os # system("clear")
 import random # randrange
 
 '''
-1 2 3   1, 2, 3 - first raw, etc.
-4 5 6   1, 4, 7 - first column, etc.
-7 8 9   1, 5, 9 - left-to-right diagonal, 3, 5, 7 - right-to-left diagonal
+For input used next table:
+ ____________________
+|      |      |      |
+|      |      |      |
+|  0   |  1   |  2   |
+|      |      |      |
+|______|______|______|
+|      |      |      |
+|      |      |      |
+|  3   |  4   |  5   |
+|      |      |      |
+|______|______|______|
+|      |      |      |
+|      |      |      |
+|  6   |  7   |  8   |
+|      |      |      |
+|______|______|______|
+
+And next symbols:
+ ____	 ____
+|    |  |	 |
+|\  /|	| __ |
+| \/ |	||	||
+| /\ |	||  ||
+|/  \|	||__||
+|____|	|____|
 '''
 
 class UsedFieldError(Exception):
@@ -22,12 +45,99 @@ class gameOver(Exception):
         else:
             self.message = "It's Draw"
 
-matrix = [[str(i + 1 + (j * 3)) for i in range(3)] for j in range(3)]
+# function for class 'Table' (draw)
+def get_3items(lst):
+	i = 0
+	length = len(lst)
+
+	while i < length:
+		try:
+			yield lst[i], lst[i + 1], lst[i + 2]
+			i += 3
+		except IndexError:
+			return
+
+class Table():
+	def __init__(self):
+		self.matrix = [str(i) for i in range(1, 10)]
+		self.cross = [
+			'      |',
+			' \  / |',
+			'  \/  |',
+			'  /\  |',
+			'_/__\_|'
+		]
+		self.zero = [
+			'  __  |',
+			' |  | |',
+			' |  | |',
+			' |__| |',
+			'______|'
+		]
+
+	def add(self, char, field_num):
+		if not 0 < field_num < 10:
+			raise IndexError
+
+		if self.matrix[field_num - 1] == 'O' or self.matrix[field_num - 1] == 'X':
+			raise UsedFieldError
+
+		self.matrix[field_num - 1] = char
+
+	def draw(self):
+		fields = []
+
+		for i in range(9):
+			if self.matrix[i] == 'X':
+				fields.append(self.cross)
+			elif self.matrix[i] == 'O':
+				fields.append(self.zero)
+			else:
+				fields.append([
+					'      |',
+					'      |',
+					'  {}   |'.format(i + 1),
+					'      |',
+					'______|'
+				])
+
+		# output
+		print(' ____________________ ')
+
+		for field1, field2, field3 in get_3items(fields):
+			for i in range(5):
+				print('|' + field1[i] + field2[i] + field3[i])
+
+	def win(self):
+		# if someone is winner
+		if self.matrix[0] == self.matrix[1] == self.matrix[2]:
+			raise gameOver(self.matrix[0])
+		elif self.matrix[3] == self.matrix[4] == self.matrix[5]:
+			raise gameOver(self.matrix[3])
+		elif self.matrix[6] == self.matrix[7] == self.matrix[8]:
+			raise gameOver(self.matrix[6])
+		elif self.matrix[0] == self.matrix[3] == self.matrix[6]:
+			raise gameOver(self.matrix[0])
+		elif self.matrix[1] == self.matrix[4] == self.matrix[7]:
+			raise gameOver(self.matrix[1])
+		elif self.matrix[2] == self.matrix[5] == self.matrix[8]:
+			raise gameOver(self.matrix[2])
+		elif self.matrix[0] == self.matrix[4] == self.matrix[8]:
+			raise gameOver(self.matrix[0])
+		elif self.matrix[2] == self.matrix[4] == self.matrix[6]:
+			raise gameOver(self.matrix[2])
+
+		# if it's draw
+		for raw in self.matrix:
+			for field in raw:
+				if field != 'O' and field != 'X':
+					return
+		raise gameOver("draw")
 
 os.system("clear")
 
 print("Hello in Tic Tac Toe Game!")
-player_char = str(input("Enter character you want (X/O): "))
+player_char = str(input("Enter character you want (X/O): ")).upper()
 
 while player_char != 'O' and player_char != 'X':
     os.system("clear")
@@ -37,124 +147,41 @@ while player_char != 'O' and player_char != 'X':
 
 os.system("clear")
 
-comp_char = list(filter(lambda char: char != player_char, ['X', 'O']))[0]
+comp_char = 'X' if player_char == 'O' else 'O'
 
-def Draw():
-    for raw in matrix:
-        for num in raw:
-            print(num, end=' ')
-        print()
+gameTable = Table()
 
-def Input():
-	user_input = int(input("Enter the number of the field: "))
-
-	if not 0 < user_input < 10:
-		raise IndexError
-
-	raw = (user_input - 1) // 3
-	col = (user_input - 1) % 3
-
-	if matrix[raw][col] == 'O' or matrix[raw][col] == 'X':
-		raise UsedFieldError
-
-	matrix[raw][col] = player_char
-
-def Logic():
-    while True:
-        raw, col = [random.randrange(0, 3) for i in range(2)]
-
-        try:
-            if matrix[raw][col] == 'O' or matrix[raw][col] == 'X':
-                raise UsedFieldError
-        except UsedFieldError:
-            continue
-        else:
-            matrix[raw][col] = comp_char
-            break
-
-def Win():
-    # global gameOver
-    # global matrix_zones
-
-    '''
-    matrix_zones = [matrix[0],                                  # first raw
-                    matrix[1],                                  # second raw
-                    matrix[2],                                  # third raw
-                    [i[0] for i in matrix],                     # first column
-                    [i[1] for i in matrix],                     # second column
-                    [i[2] for i in matrix],                     # third column
-                    [matrix[i][i] for i in range(3)],           # left-to-right diagonal
-                    [matrix[0][2], matrix[1][1], matrix[2][0]]] # right-to-left diagonal
-
-    print(matrix_zones)
-
-    try:
-        winner = list(filter(lambda *args: args[0] == args[1] == args[2], matrix_zones))
-        print(winner)
-
-        if winner:
-            if winner == player_char:
-                print("You won!")
-            else:
-                print("You lost :(")
-
-            # gameOver = True
-    except IndexError:
-        print("IndexError")
-    '''
-
-    # if someone is winner
-    if matrix[0][0] == matrix[0][1] == matrix[0][2]:
-        raise gameOver(matrix[0][0])
-    elif matrix[1][0] == matrix[1][1] == matrix[1][2]:
-        raise gameOver(matrix[1][0])
-    elif matrix[2][0] == matrix[2][1] == matrix[2][2]:
-        raise gameOver(matrix[2][0])
-    elif matrix[0][0] == matrix[1][0] == matrix[2][0]:
-        raise gameOver(matrix[0][0])
-    elif matrix[0][1] == matrix[1][1] == matrix[2][1]:
-        raise gameOver(matrix[0][1])
-    elif matrix[0][2] == matrix[1][2] == matrix[2][2]:
-        raise gameOver(matrix[0][2])
-    elif matrix[0][0] == matrix[1][1] == matrix[2][2]:
-        raise gameOver(matrix[0][0])
-    elif matrix[0][2] == matrix[1][1] == matrix[2][0]:
-        raise gameOver(matrix[0][2])
-
-    # if Draw
-    for raw in matrix:
-        for field in raw:
-            if field != 'O' and field != 'X':
-                return
-    raise gameOver("draw")
-
-try:
-	while True:
-		Draw()
-		try:
-			Input()
-		except UsedFieldError:
-			os.system("clear")
-			print("This field already used! Try another field", end='\n\n')
-
-			continue
-		except (IndexError, ValueError):
-			os.system("clear")
-			print("Wrong symbol! Try again.", end='\n\n')
-
-			continue
+while True:
+	try:
+		gameTable.draw()
+		gameTable.add(player_char, int(input("Enter the number of the field: ")))
 
 		os.system("clear")
 
-		Draw()
-		Win()
-		Logic()
+		gameTable.draw()
+		gameTable.win()
+
+		# Logic
+		while True:
+			try:
+				gameTable.add(comp_char, int(random.randrange(1, 10)))
+			except UsedFieldError:
+				continue
+			else:
+				break
 
 		os.system("clear")
 
-		Draw()
-		Win()
+		gameTable.draw()
+		gameTable.win()
 
 		os.system("clear")
-except gameOver as g:
-	print(g.message)
+	except gameOver as g:
+		print(g.message)
+		break
+	except UsedFieldError:
+		os.system("clear")
+		print("This field already used! Try another field", end='\n\n')
+	except (IndexError, ValueError):
+		os.system("clear")
+		print("Wrong symbol! Try again.", end='\n\n')
